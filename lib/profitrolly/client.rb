4e40edfe28ec291684d4bd1f4e4c14db9fd37b86
Pavel Bozhenik <bozhenik@gmail.com>
2015-01-19 12:29:29 +0300

    Add available method

--- lib/profitrolly/client.rb
+++ lib/profitrolly/client.rb
@@ -7,7 +7,8 @@ module Profitrolly
 
     @@host = 'http://profitrolly.com/merchant_api/v1'.freeze
     @@methods = {
-        activate: 'activate'
+        activate: 'activate',
+        available: 'coupons'
     }.freeze
 
     def initialize(token:)
@@ -16,7 +17,7 @@ module Profitrolly
 
     def activate(code:)
       @data = { token: @token, code: code }
-      resp = response :activate
+      resp = post :activate
       resp.body
     rescue Faraday::ResourceNotFound => error
       { error: error.message, code: 404 }
@@ -24,19 +25,34 @@ module Profitrolly
       { error: error.message, code: 401 }
     end
 
+    def available?(code)
+      resp = get :available, code
+      resp.body.has_key? ('code') and resp.body.has_key? ('owned_at') and resp.body.has_key? ('used_at')\
+      and resp.body['code'] == code and Time.parse(resp.body['owned_at']) < Time.now and resp.body['used_at'] == ''
+    end
+
     private
 
-    def response(method)
-      conn = Faraday.new(url: @@host) do |faraday|
+    def connection
+      Faraday.new(url: @@host) do |faraday|
         faraday.request  :json
         faraday.response :json, :content_type => /\bjson$/
         # faraday.response :logger
         # faraday.response :raise_error
         faraday.adapter  Faraday.default_adapter
       end
-      conn.post do |req|
+    end
+
+    def post(method)
+      connection.post do |req|
         req.url "#{@@methods[method]}"
         req.body = @data.to_json
+      end
+    end
+
+    def get(method, parameter)
+      connection.get do |req|
+        req.url "#{@@methods[method]}/#{parameter}", token: @token
       end
     end
 
